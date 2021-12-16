a45f58ab6b7d5389b4eaef4205ac958b5f0dfd7a
itspacchu <37984032+itspacchu@users.noreply.github.com>
2021-12-16 18:47:01 +0000

    controller support

--- CameraPos.gd
+++ CameraPos.gd
@@ -5,7 +5,8 @@ var targetNode
 var camera;
 var springArmNode
 export (float) var lerpSpeed = 0.05
-
+var xangle = 0;
+var yangle = 0;
 func remap(val,inlow,inhigh,outlow,outhigh):
 	return (val - inlow) / (inhigh - inlow) * (outhigh - outlow) + outlow
 
@@ -16,9 +17,7 @@ func _ready():
 	print(targetNode.get("speed"))
 func _physics_process(delta):
 	self.translation = lerp(self.translation,targetNode.translation,lerpSpeed*delta)
-	springArmNode.spring_length = lerp(springArmNode.spring_length, remap(targetNode.get("speed"),0,50,5,50),delta*10)
+	springArmNode.spring_length = lerp(springArmNode.spring_length, remap(targetNode.get("speed"),0,50,5,30),delta*10)
 	
-	if(Input.is_key_pressed(KEY_A)):
-		self.rotation.y = lerp(self.rotation.y,self.rotation.y - delta,0.5)
-	elif(Input.is_key_pressed(KEY_D)):
-		self.rotation.y = lerp(self.rotation.y,self.rotation.y + delta,0.5)
+	self.rotate_object_local(Vector3(1,0,0),delta*( -Input.get_action_strength("cam_xp") + Input.get_action_strength("cam_xn")))
+	self.rotate_object_local(Vector3(0,1,0),delta*( -Input.get_action_strength("cam_yp") + Input.get_action_strength("cam_yn")))
--- rocket.gd
+++ rocket.gd
@@ -8,7 +8,7 @@ var landingmode:bool = false
 
 var target_rot = Vector3(0,0,0)
 var target_pos = Vector3(0,1,0)
-var thrust_vec = Vector3(0.1,0,0.1)
+var thrust_vec = Vector3(0.0,0,0.0)
 
 var PID_X = null
 var PID_Z = null
@@ -24,52 +24,47 @@ func _ready():
 	PID_X = preload("res://PID.gd").new(1, 0.2, 0.5)
 	PID_Z = preload("res://PID.gd").new(1, 0.2, 0.5)
 	#thrust vectoring
-	PID_tv_x = preload("res://PID.gd").new(0.3, 0.2, 0.1)
-	PID_tv_z = preload("res://PID.gd").new(0.3, 0.2, 0.1)
+	PID_tv_x = preload("res://PID.gd").new(0.5, 0.1, 0.1)
+	PID_tv_z = preload("res://PID.gd").new(0.5, 0.1, 0.1)
 	#height calib
-	PID_Ypos = preload("res://PID.gd").new(1, 0.1, 0.5)
-	
-	PID_tv_x._set_range(-0.5,0.5)
-	PID_tv_z._set_range(-0.5,0.5)
-
-	PID_Ypos.minimum_clamp = 0; # can't get negative thrust right
+	PID_Ypos = preload("res://PID.gd").new(0.75, 0.35, 0.75)
+	PID_tv_x._set_range(-0.3,0.3)
+	PID_tv_z._set_range(-0.3,0.3)
+	PID_Ypos._set_range(0,1)
+	#PID_Ypos.minimum_clamp = 0; # can't get negative thrust right
 
 	
 func _process(delta):
+	DebugDraw.draw_box(target_pos,Vector3.ONE/2,Color(0,1,1))
 	$Booster/OmniLight.light_energy = 5*int($Booster.emitting)
 	if(Input.is_key_pressed(KEY_Y)):
 		self.apply_torque_impulse(Vector3(rand_range(-3,3),0,rand_range(-3,3)))
 	
-	if(Input.is_key_pressed(KEY_F)):
-		target_pos.x = lerp(target_pos.x,0,delta)
-		target_pos.z = lerp(target_pos.z,0,delta)
-		landingmode = true
-		
-
-func _physics_process(delta):
-	$Booster.translation = thrustloc
-	$Booster.rotation = thrust_vec
 	var in_v = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	var in_h = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
-	if(Input.is_key_pressed(KEY_Q)):
-		target_pos.y += delta*10
-	if(Input.is_key_pressed(KEY_E)):
-		target_pos.y -= delta*3
 	
-	if(landingmode and Input.is_key_pressed(KEY_K)):
-		target_pos = lerp(target_pos,Vector3.ZERO,delta)
+	if(Input.get_action_strength("tg_up")):
+		target_pos.y += Input.get_action_strength("tg_up")*thrustForce*delta/4
+	if(Input.get_action_strength("tg_down")):
+		target_pos.y -= Input.get_action_strength("tg_down")*thrustForce*delta/4
+	
+	target_pos.x -= in_h*delta*10
+	target_pos.z -= in_v*delta*10
+	
+	if(landingmode || Input.is_key_pressed(KEY_K)):
+		if(landingmode==false):
+			landingmode = true;
+		target_pos.x =  lerp(target_pos.x,0,0.75*delta)
+		target_pos.z =  lerp(target_pos.z,0,0.75*delta)
+		target_pos.y =  lerp(target_pos.y,0,0.3*delta)
 		
-	target_pos.x -= in_h*delta*30
-	target_pos.z += in_v*delta*30
-	
-	DebugDraw.draw_box(target_pos,Vector3.ONE,Color(0,1,0))
-
+func _physics_process(delta):	
 	self.speed =  self.linear_velocity.length()
 	
 
-	_apply_corr_torque(delta)
+	_apply_corr(delta)
 
-func _apply_corr_torque(delta):
+func _apply_corr(delta):
 	var rx = self.rotation.x
 	var rz = self.rotation.z
 	
@@ -84,20 +79,21 @@ func _apply_corr_torque(delta):
 	var err_pos_z:float = target_pos.z - pz;
 	var err_pos_y:float = target_pos.y - height;
 	
-	var pidx = PID_X._update(err_rot_x,delta)
-	var pidz = PID_Z._update(err_rot_z,delta)
-	
 	thrust_vec.x = PID_tv_x._update(-err_pos_x,delta)
 	thrust_vec.z = PID_tv_z._update(-err_pos_z,delta)
 	
+	var pidx = PID_X._update(err_rot_x,delta)
+	var pidz = PID_Z._update(err_rot_z,delta)
+	
+	
+	
 	
 	var pidh = PID_Ypos._update(err_pos_y,delta)
-	$RichTextLabel.text = "pidx : " + str(pidx) +"\npidz  : " + str(pidz) +"\ntv.x : " + str(thrust_vec.x) +"\ntv.y  : " + str(thrust_vec.y)
+	$RichTextLabel.text = "pidx : " + str(pidx) +"\npidz  : " + str(pidz) +"\ntv.x : " + str(thrust_vec.x) +"\ntv.y  : " + str(thrust_vec.z)
 	self.apply_torque_impulse(sideForce*pidx*transform.basis.x*delta)
 	self.apply_torque_impulse(sideForce*pidz*transform.basis.z*delta)
 	self.add_force(thrustForce*pidh*transform.basis.y,thrustloc + thrust_vec)
-	
-	DebugDraw.draw_ray_3d(self.translation + Vector3(0,1,0),Vector3(50*pidx,pidh,50*pidz),1,Color(1,0.5,0.5))
+
 	
 	animate_thrusters(pidx,pidz)
 		
@@ -118,7 +114,14 @@ func animate_thrusters(x,z):
 		$SideXn.emitting = true;
 	else:
 		$SideXn.emitting = false;		
-	if(self.linear_velocity):
+	if(self.linear_velocity.length() > 0.1  || (self.translation.y > 1.2)):
+		print(self.linear_velocity.length())
 		$Booster.emitting= true
+	else:
+		$Booster.emitting= false
+	$Booster.translation = thrustloc
+	var tv = Vector3(thrust_vec.z,-1,thrust_vec.x)
+	$Booster.rotation = tv
+	$Booster.lifetime = speed/25
 	
 	
